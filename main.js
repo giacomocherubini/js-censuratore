1b19d9d1c6cea62f9a4ba683d0862e89fe3c7dfb
giacomocherubini <giacomo.cherubini1@gmail.com>
2019-05-06 10:39:21 +0200

    fine

--- main.js
+++ main.js
@@ -2,21 +2,35 @@
 // - Restituisce il testo con xxx al posto delle parole censurate.
 // - Stampa un “badword index” calcolato come il numero di parole censurate su il numero di parole totali
 
-var testo = prompt("inserisci il testo");
-var contenitore_testo = [];
-testo == contenitore_testo;
-console.log(contenitore_testo[i]);
-var parole_censurate = prompt("inserisci parole da censurare");
-var contenitore_censure = [];
-parole_censurate == contenitore_censure;
 
-function sostituzione(contenitore_testo, parole_censurate){
-  for(var i = 0; i < contenitore_testo.length; i++) {
-    if(contenitore_testo[i] == parole_censurate) {
-        var res = parole_censurate.replace("","W3Schools");
-        return res;
-    }
+// Creo una variabili una variabile prompt per inserire il testo
+var testo = prompt("inserire il testo");
+
+// trasformare il testo in minuscolo
+testo = testo.toLowerCase();
+
+var contenitore_testo = testo.split(" ");
+
+
+// creo un array con delle parole censurate
+var censurate = ["ciao", "buongiorno", "cane", "gatto"];
+console.log(testo, censurate);
+// creo una variabile per contare le parole censurate
+var contatore_censurate = 0;
+
+for (var i = 0; i < contenitore_testo.length; i++) {
+  if (censurate.includes(contenitore_testo[i])) {
+    contenitore_testo[i] = "xxx";
+    contatore_censurate ++;
   }
 }
 
-    console.log(contenitore_testo);
+console.log(contenitore_testo.join(" "));
+console.log(contatore_censurate);
+
+// creo un un indice parole censurate / testo
+var badword_index = 0;
+if (contenitore_testo.length != "0") {
+  badword_index =  contatore_censurate/contenitore_testo.length
+}
+console.log(badword_index);
